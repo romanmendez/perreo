ef4839a4fc8be4660ab66c7ff5dc9ceb94f6b740
Román Méndez <mendezbeck@gmail.com>
2023-10-11 11:35:58 +0200

    addDogToOwner mutation

--- src/components/owner/mutation.js
+++ src/components/owner/mutation.js
@@ -2,6 +2,7 @@ const { updateResolver } = require("../../../graphql/defaults.js");
 
 const OwnerMutationType = `
   createOwner(input: OwnerInput): Owner!
+  addDogToOwner(ownerId: String!, dogId: String!): Owner!
   updateOwner(id: String!, input: OwnerInput): Owner!
 `;
 
@@ -11,6 +12,19 @@ const OwnerMutationResolver = {
     if (exists) throw new Error("A user with this DNI already exists.");
 
     return await context.model.owner.create(args);
+  },
+  addDogToOwner: async (parent, args, context) => {
+    const owner = await context.model.owner.findById(args.ownerId);
+    const dog = await context.model.dog.findById(args.dogId);
+    const dogOwners = [...new Set([...dog.owners, args.ownerId])];
+    const ownerDogs = [...new Set([...owner.dogs, args.dogId])];
+
+    await updateResolver("dog", { id: args.dogId, owners: dogOwners }, context);
+    return await updateResolver(
+      "owner",
+      { id: args.ownerId, dogs: ownerDogs },
+      context
+    );
   },
   updateOwner: async (parent, args, context) =>
     await updateResolver("owner", args, context),
