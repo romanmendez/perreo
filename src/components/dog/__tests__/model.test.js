a0555243c3cc7d777d56b039ebdb163ece135969
Román Méndez <mendezbeck@gmail.com>
2023-10-16 11:58:15 +0200

    working on first test

--- src/components/dog/__tests__/model.test.js
+++ src/components/dog/__tests__/model.test.js
@@ -1,12 +1,13 @@
+const mongoose = require("mongoose");
 const { ApolloServer, gql } = require("apollo-server");
 const { createTestClient } = require("apollo-server-testing");
-const mongoose = require("mongoose");
-const { fakerES: faker } = require("@faker-js/faker");
-const { fakeDogBuilder } = require("@test/builders");
+const { fakeDogBuilder, getSchemaFields } = require("@test");
+const cases = require("jest-in-case");
 const resolvers = require("@graphql/resolvers");
 const model = require("@db/models");
 const utils = require("@utils/index");
 const types = require("@graphql/types");
+const { DogModel } = require("@dog");
 
 const server = new ApolloServer({
   typeDefs: types,
@@ -20,41 +21,59 @@ const server = new ApolloServer({
   },
 });
 
-beforeAll(() =>
-  mongoose.connect("mongodb://localhost:27017/perreo_test", {
+const { query, mutate } = createTestClient(server);
+
+beforeAll(async () => {
+  await mongoose.connect("mongodb://localhost:27017/perreo_test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  })
-);
+  });
+});
+
 afterAll(async () => {
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
 });
 
-const { query, mutate } = createTestClient(server);
+const [fakeDog] = fakeDogBuilder(1);
+const createDogMutation = gql`
+  mutation CreateDog($input: DogInput) {
+    createDog(input: $input) {
+      id
+    }
+  }
+`;
 
-test("dogs used passes resolver", async () => {
-  const [fakeDog] = fakeDogBuilder(1);
-  const createDogMutation = gql`
-    mutation CreateDog($input: DogInput) {
-      createDog(input: $input) {
+const dogQuery = gql`
+  query DogQuery {
+    dogs {
+      name
+      breed
+      sex
+      dateOfBirth
+      fixed
+      heat
+      chip
+      scan
+      isActive
+      notes {
+        key
+        value
+        isActive
+      }
+      vaccines {
         name
-        breed
-        sex
-        dateOfBirth
-        profilePic
-        fixed
-        heat
-        chip
-        scan
+        dateAdministered
+        nextDue
+      }
+      owners {
+        firstName
+        lastName
+        email
+        phone
+        dni
       }
     }
-  `;
-
-  const { data } = await mutate({
-    mutation: createDogMutation,
-    variables: { input: newDog },
-  });
-  expect(data.createDog.name).toEqual(fakeDog.name);
-});
+  }
+`;
