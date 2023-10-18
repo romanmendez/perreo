fd2cda95d62696b65d3bc66e001988870f55b1b7
Román Méndez <mendezbeck@gmail.com>
2023-10-18 12:46:56 +0200

    move test server to tst-server.js

--- src/components/dog/__tests__/mutations.test.js
+++ src/components/dog/__tests__/mutations.test.js
@@ -1,30 +1,12 @@
-const { ApolloServer, gql } = require("apollo-server");
-const model = require("@db/models");
-const resolvers = require("@graphql/resolvers");
-const utils = require("@utils/index");
-const types = require("@graphql/types");
 const { fakeDogBuilder } = require("@test/builders");
-const { createTestClient } = require("apollo-server-testing");
+const { query, mutate, gql } = require("@test/server");
+const model = require("@db/models");
 
 jest.mock("@db/models");
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
-
-const server = new ApolloServer({
-  typeDefs: types,
-  resolvers,
-  context: ({ req }) => {
-    return {
-      ...req,
-      utils,
-      model,
-    };
-  },
-});
-
-const { query, mutate } = createTestClient(server);
 
 describe("createDog mutations", () => {
   const dogs = fakeDogBuilder(2);
